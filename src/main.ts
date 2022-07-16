78a486f47786361f88cbea065201bf3b83d5b246
Thamer Ayachi <thamerjak3@gmail.com>
2022-07-16 18:37:54 +0100

    adding swaggir v 1.0.0

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.enableCors({ origin: '*', credentials: true });
+
+  // swagger
+  const options = new DocumentBuilder()
+    .setTitle('Nest JWT Authentication')
+    .setDescription('Rest API with JWT token key for Authentication')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
